b7bd64c9b4282f9d62c4bb73602096fd302afdc6
DellNotebook <liu_zhao_feng_alex@163.com>
2024-01-12 23:13:41 +0800

    完善install.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,1 +1,1 @@
-lm_eval --model hf --model_args pretrained=IDEA-CCNL/Wenzhong-GPT2-110M --tasks arc_challenge,hellaswag,piqa --device cuda:0 --batch_size 8
+lm_eval --model hf --model_args pretrained=IDEA-CCNL/Wenzhong-GPT2-110M --tasks arc_challenge,hellaswag,piqa --device cpu --batch_size 8
--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
-cd lm-evaluation-harness && pip install -e .
-cd optimum-benchmark && pip install -e .
+# 判断当前目录下是否有 lm-evaluation-harness 文件夹，如果没有的话，执行git clone
+if [ ! -d "lm-evaluation-harness" ]; then
+  git clone https://github.com/EleutherAI/lm-evaluation-harness
+fi
+# 判断 pip list 中是否有 lm-eval，如果没有的话，执行 pip install -e .
+if ! pip list | grep -q 'lm-eval'; then
+  cd lm-evaluation-harness && pip install -e . && cd ..
+fi
+
+# 判断当前目录下是否有 optimum-benchmark 文件夹，如果没有的话，执行git clone
+if [ ! -d "optimum-benchmark" ]; then
+  git clone https://github.com/huggingface/optimum-benchmark.git
+fi
+# 判断 pip list 中是否有 optimum-benchmark，如果没有的话，执行 pip install -e .
+if ! pip list | grep -q 'optimum-benchmark'; then
+  cd optimum-benchmark && pip install -e . && cd ..
+fi
