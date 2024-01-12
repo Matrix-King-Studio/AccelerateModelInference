ab28209f1533a90b8d090dbb7a84729208b403d7
DellNotebook <liu_zhao_feng_alex@163.com>
2024-01-12 23:31:55 +0800

    修正install.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,1 +1,7 @@
-lm_eval --model hf --model_args pretrained=IDEA-CCNL/Wenzhong-GPT2-110M --tasks arc_challenge,hellaswag,piqa --device cpu --batch_size 8
+lm_eval --model hf \
+    --model_args pretrained=IDEA-CCNL/Wenzhong-GPT2-110M \
+    --tasks hellaswag,piqa \
+    --device cpu \
+    --batch_size auto \
+    --output_path ./output \
+    --log_samples
