403283584140b8eec5ba8530cadb6394b35a1109
DellNotebook <liu_zhao_feng_alex@163.com>
2024-01-12 23:44:24 +0800

    换用gpt2

--- evaluate.sh
+++ evaluate.sh
@@ -1,6 +1,6 @@
 lm_eval --model hf \
-    --model_args pretrained=IDEA-CCNL/Wenzhong-GPT2-110M \
-    --tasks hellaswag,piqa \
+    --model_args pretrained=gpt2,trust_remote_code=True \
+    --tasks arc_challenge,hellaswag,piqa \
     --device cpu \
     --batch_size auto \
     --output_path ./output \
