b26f828b1aa65eed2490b7d527208ba5fc356cc6
Alex_996 <45281765+koking0@users.noreply.github.com>
2024-01-13 07:48:16 +0000

    install.sh增加安装Python依赖

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 echo "CPU 相关信息"
 lscpu
 
+echo "CPU 内存大小"
+free -h | grep Mem | awk '{print $2}'
+
 echo "升级 pip"
 python -m pip install --upgrade pip
+
+echo "安装 Python 依赖"
+pip install -r requirements.txt
 
 # 判断当前目录下是否有 lm-evaluation-harness 文件夹，如果没有的话，执行git clone
 if [ ! -d "lm-evaluation-harness" ]; then
