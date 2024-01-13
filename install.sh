0a9880dae4542df961633f73874df7281dbd14b2
Alex_996 <45281765+koking0@users.noreply.github.com>
2024-01-13 13:05:46 +0000

    optimum-benchmark转为通过pip安装

--- install.sh
+++ install.sh
@@ -10,26 +10,23 @@ python -m pip install --upgrade pip
 echo "安装 Python 依赖"
 pip install -r requirements.txt
 
+# 获取当前路径并保存到变量中
+current_path=$(pwd)
+
 # 判断当前目录下是否有 lm-evaluation-harness 文件夹，如果没有的话，执行git clone
 if [ ! -d "lm-evaluation-harness" ]; then
-  git clone https://github.com/EleutherAI/lm-evaluation-harness
-fi
-# 判断 pip list 中是否有 lm-eval，如果没有的话，执行 pip install -e .
-if ! pip list | grep 'lm-eval'; then
-  cd lm-evaluation-harness && pip install -e . && cd ..
+  git clone https://github.com/EleutherAI/lm-evaluation-harness && cd lm-evaluation-harness && pip install -e .
 fi
 
-# 判断当前目录下是否有 optimum-benchmark 文件夹，如果没有的话，执行git clone
-if [ ! -d "optimum-benchmark" ]; then
-  git clone https://github.com/huggingface/optimum-benchmark.git
-fi
-# 判断 pip list 中是否有 optimum-benchmark，如果没有的话，执行 pip install -e .
-if ! pip list | grep 'optimum-benchmark'; then
-  cd optimum-benchmark && pip install -e . && cd ..
-fi
+cd "$current_path"
 
 # 判断当前目录下是否有 llama.cpp 文件夹，如果没有的话，执行git clone
 if [ ! -d "llama.cpp" ]; then
   git clone https://github.com/ggerganov/llama.cpp && cd llama.cpp && make
-  cd /tmp/ && git clone https://huggingface.co/Qwen/Qwen-7B
 fi
+
+cd "$current_path"
+
+cd /tmp/ && git clone https://huggingface.co/Qwen/Qwen-7B
+
+cd "$current_path"
--- lm-evaluate.sh
+++ lm-evaluate.sh
@@ -2,5 +2,5 @@ lm_eval --model hf \
     --model_args pretrained=/tmp/Qwen-7B/,trust_remote_code=True \
     --tasks arc_challenge,hellaswag,piqa \
     --device cpu \
-    --batch_size auto \
+    --batch_size 64 \
     --output_path ./lm-eval-output
