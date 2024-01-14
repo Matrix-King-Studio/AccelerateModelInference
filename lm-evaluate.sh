bf5baef9cfadecf02e96e63de7c03bec5b59b147
Alex_996 <45281765+koking0@users.noreply.github.com>
2024-01-14 09:13:33 +0000

    123

--- lm-evaluate.sh
+++ lm-evaluate.sh
@@ -1,6 +1,8 @@
-lm_eval --model hf \
-    --model_args pretrained=Qwen/Qwen-7B,trust_remote_code=True,load_in_8bit=True \
-    --tasks arc_challenge,hellaswag,piqa \
-    --device cpu \
-    --batch_size 16 \
-    --output_path ./lm-eval-output
+# lm_eval --model hf \
+#     --model_args pretrained=Qwen/Qwen-7B,trust_remote_code=True,load_in_8bit=True \
+#     --tasks arc_challenge,hellaswag,piqa \
+#     --device cpu \
+#     --batch_size 16 \
+#     --output_path ./lm-eval-output
+
+lm_eval --model Llama.cpp --model_args pretrained=/tmp/Qwen-7B-gguf/qwen7b-q4_0.gguf --tasks arc_challenge,hellaswag,piqa --device cpu --batch_size 16 --output_path ./lm-eval-output
